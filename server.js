38561c5e120a4983858e59aa16b91c5d66451049
Crispino <gabriel.crispino@hotmail.com>
2017-09-21 18:30:57 -0300

    Correct bug on function readDirR

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const Path = require('path');
+const path = require('path');
 const download = require('./download');
 
 const app = express();
