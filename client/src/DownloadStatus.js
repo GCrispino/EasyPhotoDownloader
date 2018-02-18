ec35f968558b3a09029f11100e5793b8e1e1cdc5
Crispino <gabriel.crispino@hotmail.com>
2018-02-18 12:30:27 -0300

    Convert <DownloadStatus /> to a functional stateless component

--- client/src/DownloadStatus.js
+++ client/src/DownloadStatus.js
@@ -1,31 +1,26 @@
 const React = require('react');
 const Loader = require('react-loader');
 
-class DownloadStatus extends React.Component{
-	getDownloadedPhotos(){
-		const {photosToDownload} = this.props;
-
-		return photosToDownload
+function getDownloadedPhotos(photosToDownload){
+	return photosToDownload
 		.map(album => album.photos)
-		.reduce((a,b) => a.concat(b),[])
+		.reduce((a, b) => a.concat(b), [])
 		.filter(photo => photo.downloaded);
-	}
-
-	render(){
-		const nAllPhotos = 
-			this.props.photosToDownload.map(album => album.photos)
-			.reduce((a,b) => a.concat(b),[])
-			.length;
-
-		const downloadedPhotos = this.getDownloadedPhotos();
-			return (
-				<div className="downloadStatus">
-					<Loader /> 
-					<h3>Downloading...(please wait)</h3>
-					<h4>{downloadedPhotos.length} / {nAllPhotos} photos</h4>
-				</div>
-			);
-		}
 }
 
-module.exports = DownloadStatus;
+module.exports = function (props){
+	const {photosToDownload} = props;
+	const nAllPhotos =
+		photosToDownload.map(album => album.photos)
+			.reduce((a, b) => a.concat(b), [])
+			.length;
+
+	const downloadedPhotos = getDownloadedPhotos(photosToDownload);
+	return (
+		<div className="downloadStatus">
+			<Loader />
+			<h3>Downloading...(please wait)</h3>
+			<h4>{downloadedPhotos.length} / {nAllPhotos} photos</h4>
+		</div>
+	);
+};
