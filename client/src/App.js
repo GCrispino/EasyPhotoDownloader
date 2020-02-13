0e1f6a92e61d2496d36ff79c922c81229567a054
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-13 13:52:23 -0300

    Update to FB graph API 6.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,101 +1,114 @@
-const React = require('react');
-const UserPage = require('./UserPage');
-const LoginPage = require('./LoginPage');
+const React = require("react");
+const UserPage = require("./UserPage");
+const LoginPage = require("./LoginPage");
 
-class App extends React.Component{
-	constructor(props){
-		super(props);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    if (
+      window.location.hostname === "gcrispino.github.io" &&
+      window.location.protocol === "http:"
+    )
+      window.location.protocol = "https:";
 
-		if (
-			window.location.hostname === 'gcrispino.github.io' 
-			&& 
-			window.location.protocol === 'http:'
-		)
-			window.location.protocol = 'https:';
+    const appId = "1684103011832056";
 
-		const appId = '1684103011832056';
-		
-		
-		this.state = {
-			appId,
-			loggedIn: false,
-			sdkLoaded: true
-		};
+    this.state = {
+      appId,
+      loggedIn: false,
+      sdkLoaded: true
+    };
 
+    this.statusChangeCallback = this.statusChangeCallback.bind(this);
+  }
 
-		this.statusChangeCallback = this.statusChangeCallback.bind(this);
-	}
+  componentDidMount() {
+    const self = this;
 
-	componentDidMount(){
-		const self = this;
+    (function(d, s, id) {
+      let js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = `//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v6.0&appId=${self.state.appId}`;
+      fjs.parentNode.insertBefore(js, fjs);
 
-		(function(d, s, id) {
-			let js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
-			js.src = `//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.10&appId=${self.state.appId}`;
-			fjs.parentNode.insertBefore(js, fjs);
+      js.addEventListener("load", () => {
+        self.setState({
+          sdkLoaded: true,
+          FB: window.FB
+        });
+      });
+    })(document, "script", "facebook-jssdk");
+  }
 
-			js.addEventListener('load',() => {
-				self.setState({
-					sdkLoaded: true,
-					FB : window.FB
-				});
-			});
-		})(document, 'script', 'facebook-jssdk');
-	}
+  // This is called with the results from from FB.getLoginStatus().
+  statusChangeCallback(response) {
+    // app know the current login status of the person.
+    console.log("statuschange");
+    if (response.authResponse) {
+      const userId = response.authResponse.userID,
+        accessToken = response.authResponse.accessToken;
 
-	// This is called with the results from from FB.getLoginStatus().
-	statusChangeCallback(response) {
-		// app know the current login status of the person.
-		console.log('statuschange');
-		
-		// The response object is returned with a status field that lets the
-		// Full docs on the response object can be found in the documentation
-		// for FB.getLoginStatus().
-		if (response.status === 'connected') {
-			// Logged into your app and Facebook.
-			const userId = response.authResponse.userID,
-				accessToken = response.authResponse.accessToken;
-	
-			console.log('connected!');
-			console.log(userId,accessToken);
+      console.log("connected!");
+      console.log(userId, accessToken);
 
-			this.setState({
-				loggedIn: true,
-				userId,
-				accessToken
-			});
+      this.setState({
+        loggedIn: true,
+        userId,
+        accessToken
+      });
+    } else {
+      console.log("User cancelled login or did not fully authorize.");
+    }
+    // The response object is returned with a status field that lets the
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    //    if (response.status === "connected") {
+    //      // Logged into your app and Facebook.
+    //      const userId = response.authResponse.userID,
+    //        accessToken = response.authResponse.accessToken;
+    //
+    //      console.log("connected!");
+    //      console.log(userId, accessToken);
+    //
+    //      this.setState({
+    //        loggedIn: true,
+    //        userId,
+    //        accessToken
+    //      });
+    //    } else if (response.status === "not_authorized") {
+    //      // The person is logged into Facebook, but not your app.
+    //      document.getElementById("status").innerHTML =
+    //        "Please log " + "into this app.";
+    //    } else {
+    //      // The person is not logged into Facebook, so we're not sure if
+    //      // they are logged into this app or not.
+    //      document.getElementById("status").innerHTML =
+    //        "Please log " + "into Facebook.";
+    //    }
+  }
 
-		} else if (response.status === 'not_authorized') {
-			// The person is logged into Facebook, but not your app.
-			document.getElementById('status').innerHTML = 'Please log ' +
-			'into this app.';
-		} else {
-			// The person is not logged into Facebook, so we're not sure if
-			// they are logged into this app or not.
-			document.getElementById('status').innerHTML = 'Please log ' +
-			'into Facebook.';
-		}
-	}
+  handleLogin() {
+    window.FB.login(this.statusChangeCallback, { scope: "user_photos" });
+  }
 
-	handleLogin(){
-		this.state.FB.login(this.statusChangeCallback,{scope: 'user_photos'});
-	}
+  render() {
+    const { userId, accessToken } = this.state;
+    const componentToRender = this.state.sdkLoaded ? (
+      this.state.loggedIn ? (
+        <UserPage userId={userId} accessToken={accessToken} />
+      ) : (
+        <LoginPage handleLogin={this.handleLogin.bind(this)} />
+      )
+    ) : (
+      <div>Loading FB info...</div>
+    );
 
-	render(){
-		
-		const {userId,accessToken} = this.state;
-		const componentToRender = 
-			this.state.sdkLoaded ? 
-				this.state.loggedIn
-				? <UserPage userId={userId} accessToken={accessToken} />
-				: <LoginPage handleLogin={this.handleLogin.bind(this)}/>
-			: <div>Loading FB info...</div>;
-			
-		return componentToRender;
-	}
+    return componentToRender;
+  }
 }
 
 module.exports = App;
