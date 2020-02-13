3756df2fecd481ea4d0fe1ebc10ff03c53bf5e02
Gabriel Crispino <gabriel.crispino@hotmail.com>
2020-02-13 13:59:20 -0300

    Updates

---
+++ precache-manifest.a460cf4b023f957a8a5706da956ed766.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807a0d7638f44dfdc1f6c3b9ab904476",
+    "url": "/EasyPhotoDownloader/index.html"
+  },
+  {
+    "revision": "d2d2f1a2b7ad702f7e97",
+    "url": "/EasyPhotoDownloader/static/css/2.43fb1e57.chunk.css"
+  },
+  {
+    "revision": "2dc20ff786df1def8df3",
+    "url": "/EasyPhotoDownloader/static/css/main.a7d8f1c2.chunk.css"
+  },
+  {
+    "revision": "d2d2f1a2b7ad702f7e97",
+    "url": "/EasyPhotoDownloader/static/js/2.c57b13e2.chunk.js"
+  },
+  {
+    "revision": "c57feb9c84fb174064c960272ea43b79",
+    "url": "/EasyPhotoDownloader/static/js/2.c57b13e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc20ff786df1def8df3",
+    "url": "/EasyPhotoDownloader/static/js/main.351d1c88.chunk.js"
+  },
+  {
+    "revision": "737ff275856a55155ce1",
+    "url": "/EasyPhotoDownloader/static/js/runtime-main.10ba3006.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/EasyPhotoDownloader/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/EasyPhotoDownloader/static/media/icons.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/EasyPhotoDownloader/static/media/icons.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/EasyPhotoDownloader/static/media/icons.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/EasyPhotoDownloader/static/media/icons.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/EasyPhotoDownloader/static/media/icons.fee66e71.woff"
+  }
+]);
